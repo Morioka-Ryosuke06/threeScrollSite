e77edeecc5ef6e59f2dccd65545a3a9b3fd0665f
森岡良介 <corlc_r@moriokaryousukenoMacBook-Pro.local>
2022-02-28 10:29:42 +0900

    thred commit

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 
 //背景
 const textureLoader = new THREE.TextureLoader();
-const bgTexture = textureLoader.load('./bg/bg.jpg');
+const bgTexture = textureLoader.load('https://i.postimg.cc/6Qk292kN/scene-bg.jpg');
 scene.background = bgTexture
 
 //サイズ
